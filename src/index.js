6241345887408394ffa9b880cb7caff416b80b0f
HalehGhavami <79171561+HalehGhavami@users.noreply.github.com>
2022-05-30 15:16:55 +0200

    put App component inside BroweserRouter

--- src/index.js
+++ src/index.js
@@ -1,5 +1,11 @@
 import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import App from './app';
-
-ReactDOM.render(<App />, document.getElementById('root'));
+import { BrowserRouter } from 'react-router-dom';
+ReactDOM.render(
+  //all component of App have access to the object of history in browser
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
+  document.getElementById('root')
+);
