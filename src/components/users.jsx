3e7875f05a64e2fbaf3852b9e3a4fea748ad788c
HalehGhavami <79171561+HalehGhavami@users.noreply.github.com>
2022-05-30 08:30:34 +0200

    used LoadingUsers component
    "will check it later"

--- src/components/users.jsx
+++ src/components/users.jsx
@@ -1,15 +1,18 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+import LoadingUsers from './loading/loadingUsers';
 
 class Users extends Component {
   state = {
     users: [],
+    isLoading: true,
   };
   //axios return a promise
   async componentDidMount() {
     const response = await axios.get('https://reqres.in/api/users');
-    console.log(response);
-    this.setState({ users: response.data.data });
+    setTimeout(() => {
+      this.setState({ users: response.data.data, isLoading: false });
+    }, 3000);
   }
   render() {
     return (
@@ -18,39 +21,43 @@ class Users extends Component {
           Create
         </button>
         <div className="row ">
-          {this.state.users.map((user) => {
-            return (
-              <div className="col-4 text-center p-5">
-                <img
-                  src={user.avatar}
-                  style={{ borderRadius: '50%', width: '100px' }}
-                  alt="avatar"
-                />
-                <h4>
-                  {user.first_name} {user.last_name}
-                </h4>
-                <h5>{user.email}</h5>
-                <div className="row">
-                  <div className="col-6">
-                    <button
-                      onClick={this.handleUpdate}
-                      className="btn btn-sm btn-info"
-                    >
-                      Update
-                    </button>
-                  </div>
-                  <div className="col-6">
-                    <button
-                      onClick={this.handleDelete}
-                      className="btn btn-sm btn-danger"
-                    >
-                      Delete
-                    </button>
+          {this.state.isLoading ? (
+            <LoadingUsers />
+          ) : (
+            this.state.users.map((user) => {
+              return (
+                <div className="col-4 text-center p-5">
+                  <img
+                    src={user.avatar}
+                    style={{ borderRadius: '50%', width: '100px' }}
+                    alt="avatar"
+                  />
+                  <h4>
+                    {user.first_name} {user.last_name}
+                  </h4>
+                  <h5>{user.email}</h5>
+                  <div className="row">
+                    <div className="col-6">
+                      <button
+                        onClick={this.handleUpdate}
+                        className="btn btn-sm btn-info"
+                      >
+                        Update
+                      </button>
+                    </div>
+                    <div className="col-6">
+                      <button
+                        onClick={this.handleDelete}
+                        className="btn btn-sm btn-danger"
+                      >
+                        Delete
+                      </button>
+                    </div>
                   </div>
                 </div>
-              </div>
-            );
-          })}
+              );
+            })
+          )}
         </div>
       </>
     );
