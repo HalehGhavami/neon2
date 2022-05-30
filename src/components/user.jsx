c00f4cc30b6ba5b9d2c10a9b30d6d3dc13504f86
HalehGhavami <79171561+HalehGhavami@users.noreply.github.com>
2022-05-30 21:32:53 +0200

    used useState and useEffect to update state and fetch data of selected user id

--- src/components/user.jsx
+++ src/components/user.jsx
@@ -1,20 +1,35 @@
-//our parameters are located in match props
-// const User = (props) => {
-//   console.log(props.match.params);
-//   return <h1>User</h1>;
-// };
-
-// export default User;
-
+//for sending req to server to ask user details
+import axios from 'axios';
+import { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 export default function User() {
+  const [user, setUser] = useState({});
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await axios.get(`https://reqres.in/api/users/${id}`);
+      setUser(response.data.data);
+    };
+    fetchData();
+  });
+
   // We can use the `useParams` hook here to access
   // the dynamic pieces of the URL.
   let { id } = useParams();
 
   return (
-    <div>
-      <h3>ID: {id}</h3>
-    </div>
+    <>
+      <img
+        src={user.avatar}
+        style={{ borderRadius: '50%', width: '100px' }}
+        alt="avatar"
+      />
+      <h5 to={`/users/${user.id}`}>
+        <div>
+          {user.first_name} {user.last_name}
+        </div>
+      </h5>
+      <h5>{user.email}</h5>
+    </>
   );
 }
